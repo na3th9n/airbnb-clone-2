32c4725e08c404ccf3620e502535bd003ffe5d64
Nathan Xie <na3than.xie@gmail.com>
2024-08-02 20:55:37 -0400

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage";
+import Layout from "./Layout";
+import RegisterPage from "./pages/RegisterPage";
+
+function App() {
+  return (
+    <Routes>
+      <Route path={"/"} element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path="/login" index element={<LoginPage />} />
+        <Route path="/register" index element={<RegisterPage />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
